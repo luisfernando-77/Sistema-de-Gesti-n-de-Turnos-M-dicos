b225c6a2f0eb97b98dee6aedc0d5e53ea64d56db
cristianmunoz2006 <camunoz-2024a@corhuila.edu.co>
2026-04-15 22:44:43 -0500

    ...

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,20 +2,9 @@ import path from 'node:path';
 import { defineConfig } from 'prisma/config';
 import { config } from 'dotenv';
 
-// carga el .env desde la raíz
+// cargar .env desde la raíz del proyecto
 config({ path: path.join(__dirname, '..', '.env') });
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
-  datasource: {
-    url: process.env.DATABASE_URL as string,
-  },
-  migrate: {
-    async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg');
-      return new PrismaPg({
-        connectionString: process.env.DATABASE_URL as string,
-      });
-    },
-  },
 });
